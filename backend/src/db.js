866a10454eb56fa2295f45f961ab21dd90663084
hypernat <nathavichh@gmail.com>
2025-07-10 06:07:17 +0000

    metodo getAllMascotas

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,1 +1,19 @@
+const {Pool} = require('pg');
 
+const dbClient = new Pool({
+  user: 'postgres',
+  password: 'password123',
+  host: 'localhost',
+  port: 5432,
+  database: 'pethub',
+})
+
+async function getAllMascotas() {
+    const result = await dbClient.query('SELECT * FROM mascotas;');
+    return result.rows;
+}
+
+module.exports = {
+    getAllMascotas
+}; 
+     
