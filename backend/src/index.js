c70b93d3fc34a1b612b69509b5591ec6076fbc11
hypernat <nathavichh@gmail.com>
2025-07-12 01:48:12 +0000

    añado endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 3000;
 const {
     getAllMascotas,
     getOneMascota,
-} = require('./db.js')
+    createMascota,
+    deleteMascota
+} = require('./mascotas.js')
 
 // ruta health
 app.get('/index/health', (req, res) => {
@@ -18,7 +20,7 @@ app.listen(PORT, () => {
 });
 
 
-//ordenar en carpetas
+//ordenar en carpetas y MODULARIZAR
 
 //mascotas
 // get all
@@ -26,12 +28,81 @@ app.get('/index/mascotas', async (req, res) => {
   const mascotas = await getAllMascotas();
   res.json(mascotas);
 });
+
+//get one
 app.get('/index/mascotas/:id', async (req, res) => {
   const mascota = await getOneMascota(req.params.id);
+  if (!mascota) {
+    return res.status(404).json({ error: 'Upsi, mascota no encontrada :('});
+  }
   res.json(mascota);
 });
 
+/*
+PARA PROBAR
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"nombre":"nievecita","especie":"conejito","edad_estimada":2,"tamaño":"pequeño","esta_vacunado":true,"imagen":"xyz","descripcion":"le gusta la lechuga y jugar!"}' \
+  http://localhost:3000/index/mascotas
+*/
+//insert
+app.post('/index/mascotas', async (req, res) => {
 
+  if(!req.body.nombre || !req.body.especie || !req.body.edad_estimada || !req.body.tamaño || 
+     !req.body.esta_vacunado || !req.body.descripcion) {
+    return res.status(400).json({ error: 'Faltan campos obligatorios!'})
+  }
+
+  if (req.body.edad_estimada < 0 || req.body.edad_estimada > 80) {
+    return res.status(400).json({ error: 'La edad estimada debe estar entre 0 y 80 años' });
+  }
+
+  if (typeof req.body.esta_vacunado !== 'boolean') {
+  return res.status(400).json({ error: 'La respuesta de si esta vacunado debe ser solo true / false !!' });
+  }
+
+  if (!Number.isInteger(req.body.edad_estimada)) {
+  return res.status(400).json({ error: 'Edad debe ser un número entero' });
+ }
+
+  const mascota = await createMascota(
+    req.body.nombre, 
+    req.body.especie, 
+    req.body.edad_estimada, 
+    req.body.tamaño, 
+    req.body.esta_vacunado, 
+    req.body.imagen, 
+    req.body.descripcion
+  );
+  if(!mascota) {
+    return res.status(500).json({ error: 'Algo falló al crear la mascota'})
+  }
+  res.json({mascota});
+});
+
+/* PARA PROBAR
+curl --request DELETE \
+  http://localhost:3000/index/mascotas/:13
+*/
+
+app.delete('/index/mascotas/:id', async (req, res) => {
+
+    const mascota = await deleteMascota(req.params.id);
+
+    if (!mascota) {
+      return res.status(404).json({error: 'Upsi, mascota no encontrada, no se puede eliminar'})
+    }
+
+    res.json({ status: 'OK', id: mascota });
+});
+
+app.put('/index/mascotas/:id', async (req, res) => {
+  const mascota = await updateMascota(req.params.id);
+  if (!mascota) {
+    return res.status(404).json({ error: 'Upsi, mascota no encontrada :('});
+  }
+  res.json({ status: 'OK', id: mascota });
+});
 
 //user
 // GET. /user
